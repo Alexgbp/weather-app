26daeaa475837105e20846fc7f787d0b1ddf636e
Alexgbp <bboy91@bk.ru>
2025-11-03 22:20:53 +0300

    form for auth

--- src/app/authorize/page.jsx
+++ src/app/authorize/page.jsx
@@ -1,11 +1,54 @@
-import Link from 'next/link'
-import React from 'react'
-
 export default function page() {
   return (
     <div>
-      Authorize page
-      <Link href={"/"}>Back to main</Link>
+      <div>
+        <h1>Registration</h1>
+      </div>
+      <form action="">
+        <div>
+          <label>
+            Name
+            <input type="text" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Email
+            <input type="text" />
+          </label>
+        </div>
+         <div>
+          <label>
+            Date of Birth
+            <input type="date" name="bday" />
+          </label>
+        </div>
+        <div>
+          <span>Gender</span>
+          <label>
+            <input type="radio" name="gemder" value={"male"} required /> Male
+          </label>
+          <label>
+            <input type="radio" name="gemder" value={"female"} required /> Female
+          </label>
+        </div>
+        <div>
+          <label>
+            City
+            <input type="text" name="city"  />
+          </label>
+        </div>
+        <div>
+          <label>
+            Country
+            <input type="text" name="country"  />
+          </label>
+        </div>
+        <div>
+          <button type="button">Cancel</button>
+          <button type="submit">Confirm</button>
+        </div>
+      </form>
     </div>
   )
 }
